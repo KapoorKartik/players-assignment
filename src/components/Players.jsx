e8a420d3073f3098cd7a91369ca7b0fc83e9a245
kapoorkartik <kapoorkartik110@gmail.com>
2022-03-24 12:52:04 +0530

    date and time converted to local time

--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -1,10 +1,12 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import DateTimeLocal from "datetime-utc-local";
 export const Players = () => {
   const [data, setData] = useState();
   useEffect(() => {
     axios.get("https://api.npoint.io/20c1afef1661881ddc9c").then(({ data }) => {
-      // console.log("data", data.playerList);
+      // console.log("data", data);
+
       setData(data);
     });
   }, []);
@@ -23,6 +25,64 @@ export const Players = () => {
   const sortedData = data?.playerList?.sort((a, b) => {
     return +a.Value - +b.Value;
   });
+  // convert utc to specific timezone example to undertand better
+  // if (sortedData) {
+  //   // console.log("p1:", sortedData[0]);
+  //   const utc = sortedData[0]?.UpComingMatchesList[0]?.MDate;
+
+  //   // console.log("utc:", utc);
+  //   const uDate = new Date(utc).toLocaleDateString().split("/");
+  //   const time = new Date(utc).toLocaleTimeString();
+  //   // console.log("time:", time);
+  //   let temp = uDate[0];
+  //   uDate[0] = uDate[1];
+  //   uDate[1] = temp;
+  //   // console.log("uDate:", uDate);
+  //   let newDate = uDate.join("/") + " " + time;
+  //   // console.log("newDate:", newDate);
+  //   let kk = new Date(newDate);
+  //   let off = kk.getTimezoneOffset();
+  //   console.log("off:", off);
+  //   console.log("kk:", kk.getTime());
+  //   // in one day there is 86400000 ms
+  //   let rk = new Date(kk.getTime() + off * 1000 * -1 * 60 + 86400000);
+  //   console.log("rk:", rk);
+  // }
+  // converting date in local date and time and saving in localTime varible
+  sortedData?.forEach((e) => {
+    // console.log(e.UpComingMatchesList[0].MDate);
+    const utc = e.UpComingMatchesList[0].MDate;
+    if (utc.length > 0) {
+      // console.log("utc:", utc);
+      const uDate = new Date(utc).toLocaleDateString().split("/");
+      const time = new Date(utc).toLocaleTimeString();
+      // console.log("time:", time);
+      let temp = uDate[0];
+      uDate[0] = uDate[1];
+      uDate[1] = temp;
+      // console.log("uDate:", uDate);
+      let newDate = uDate.join("/") + " " + time;
+      // console.log("newDate:", newDate);
+      let correctDate = new Date(newDate);
+      let off = correctDate.getTimezoneOffset();
+      // console.log("off:", off);
+      // console.log("correctDate:", correctDate.getTime());
+      if (time === "12:00:00 AM") {
+        // in one day there is 86400000 ms
+        let newTime = new Date(
+          correctDate.getTime() + off * 1000 * -1 * 60 + 86400000
+        );
+        let newTimeString = newTime.toLocaleString();
+
+        e.localTime = newTimeString;
+      } else {
+        let newTime = new Date(correctDate.getTime() + off * 1000 * -1 * 60);
+        let newTimeString = newTime.toLocaleString();
+
+        e.localTime = newTimeString;
+      }
+    }
+  });
   return (
     <>
       <div>
@@ -37,7 +97,7 @@ export const Players = () => {
                 Upcoming Match : {e.UpComingMatchesList[0].CCode} vs{" "}
                 {e.UpComingMatchesList[0].VsCCode}
               </h4>
-              <h4>Next Match Time(UTC) : {e.UpComingMatchesList[0].MDate}</h4>
+              <h4>Next Match Time: {e.localTime}</h4>
             </>
           );
         })}
